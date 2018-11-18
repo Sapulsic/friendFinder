4b225f38c8671c85d2ec7ef191aec8dece0ca6fc
Sapulsic <carlos.barahona1995@gmail.com>
2018-11-17 21:57:10 -0600

    base files

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+    {
+        "name": "Linda",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Fred",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            4,
+            2,
+            5,
+            5,
+            4,
+            2,
+            1,
+            4,
+            5,
+            2
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ routing/apiRoutes.js
@@ -0,0 +1,18 @@
+var friends = require("../app/data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function (req,res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        if (friends.length < 5) {
+            friends.push(req.body);
+            res.json(true);
+        } else {
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+};
