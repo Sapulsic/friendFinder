54ea95729216f888bf8f7bd7b14c5228bc183105
Sapulsic <carlos.barahona1995@gmail.com>
2018-11-20 19:51:48 -0600

    Fixed api/friends

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,6 +3,7 @@ var friends = require("../app/data/friends");
 module.exports = function(app) {
 
     app.get("/api/friends", function (req,res) {
+        console.log("yo")
         res.json(friends);
     });
 
